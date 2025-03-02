aef9455e2980a88a0bf6037cf2377273169091cc
shamsunnaherchowdhuryraisa <shamsunnaherchowdhuryraisa@gmail.com>
2025-03-02 23:46:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+document.getElementById('completed-1').addEventListener('click', function () {
+    const completed1 = document.getElementById('completed-1')
+    completed1.disabled = true;
+    completed1.style.backgroundColor = 'grey';
+    const history = document.getElementById('history')
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');  // Get hours (00-23)
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes (00-59)
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds (00-59)
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    const title1 = document.getElementById('title-1').innerText
+
+    history.innerHTML = `You have Completed The Task ${title1} at ${time}`
+
+})
+document.getElementById('history-btn').addEventListener('click',function() {
+document.getElementById('history').innerText=''
+})
+document.getElementById('completed-2').addEventListener('click', function () {
+    const completed1 = document.getElementById('completed-2')
+    completed1.disabled = true;
+    completed1.style.backgroundColor = 'grey';
+    const history = document.getElementById('history')
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');  // Get hours (00-23)
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes (00-59)
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds (00-59)
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    const title1 = document.getElementById('title-1').innerText
+
+    history.innerHTML = `You have Completed The Task ${title1} at ${time}`
+
+})
+document.getElementById('history-btn').addEventListener('click',function() {
+document.getElementById('history').innerText=''
+})
+document.getElementById('completed-3').addEventListener('click', function () {
+    const completed1 = document.getElementById('completed-3')
+    completed1.disabled = true;
+    completed1.style.backgroundColor = 'grey';
+    const history = document.getElementById('history')
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');  // Get hours (00-23)
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes (00-59)
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds (00-59)
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    const title1 = document.getElementById('title-1').innerText
+
+    history.innerHTML = `You have Completed The Task ${title1} at ${time}`
+
+})
+document.getElementById('history-btn').addEventListener('click',function() {
+document.getElementById('history').innerText=''
+})
+document.getElementById('completed-4').addEventListener('click', function () {
+    const completed1 = document.getElementById('completed-4')
+    completed1.disabled = true;
+    completed1.style.backgroundColor = 'grey';
+    const history = document.getElementById('history')
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');  // Get hours (00-23)
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes (00-59)
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds (00-59)
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    const title1 = document.getElementById('title-1').innerText
+
+    history.innerHTML = `You have Completed The Task ${title1} at ${time}`
+
+})
+document.getElementById('history-btn').addEventListener('click',function() {
+document.getElementById('history').innerText=''
+})
+document.getElementById('completed-5').addEventListener('click', function () {
+    const completed1 = document.getElementById('completed-5')
+    completed1.disabled = true;
+    completed1.style.backgroundColor = 'grey';
+    const history = document.getElementById('history')
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0');  // Get hours (00-23)
+    const minutes = date.getMinutes().toString().padStart(2, '0'); // Get minutes (00-59)
+    const seconds = date.getSeconds().toString().padStart(2, '0'); // Get seconds (00-59)
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    const title1 = document.getElementById('title-1').innerText
+
+    history.innerHTML = `You have Completed The Task ${title1} at ${time}`
+
+})
+document.getElementById('history-btn').addEventListener('click',function() {
+document.getElementById('history').innerText=''
+})
